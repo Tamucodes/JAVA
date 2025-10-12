d0ee65cc84a89ad871666524590665ee21798343
Sudin <sudintamang3324@gmail.com>
2025-10-12 19:32:42 +0545

    day 1 chapter 1 and 2

---
+++ InputOutput.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        //input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine(); // taking the input
+        System.out.println(name);
+    }
+}
