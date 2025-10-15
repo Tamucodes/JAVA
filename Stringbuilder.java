d2e3b1f2929c7dfc420342c3bd674bce42bba752
Sudin <sudintamang3324@gmail.com>
2025-10-15 14:57:55 +0545

    set builder method

---
+++ Stringbuilder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Stringbuilder{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        StringBuilder st = new StringBuilder("tony");
+        System.out.println(st);
+        st.append("e");
+        System.out.print(st);
+        sc.close();
+    }
+}
