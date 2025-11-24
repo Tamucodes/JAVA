17f386ba80c88d3c6c8970a29fa122925378ea73
Sudin <sudintamang3324@gmail.com>
2025-11-24 17:53:49 +0545

    Object oriented programming

---
+++ OOP.java
@@ -0,0 +1,23 @@
+class Pen{
+    String color;
+    String type ;
+    public void write(){
+        System.out.println("writing something");
+    }
+    public void printColor(){
+        System.out.println(this.color); // 'this' keyword is used to refer to the current object
+    }
+}
+class Student{
+    String name;
+    int age;
+    
+
+}
+
+
+public class OOP{
+    public static void main(String[] args){
+     
+    }
+}
