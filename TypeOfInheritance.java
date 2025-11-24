027a385e7bb478f6908f4fb06d44a1f03feeff2f
Sudin <sudintamang3324@gmail.com>
2025-11-24 20:28:39 +0545

    types of inheritance

---
+++ TypeOfInheritance.java
@@ -0,0 +1,32 @@
+import bank;
+
+// claass Shape{
+//     public void area(){
+//         System.out.println("Displays area of shape");
+//     }
+// }
+// //example of single inheritance
+// class Triangle extends Shape{
+//     public void area(int l,int h){
+//         System.out.println(0.5*l*h);  
+//     }
+// }
+// //example of a multi-level inheritance
+// class Circle extends Shape{ 
+//     public void area(int r){
+//         System.out.println(3.14*r*r);
+//     }
+// }
+// //example of hybrid interitance
+// class EquilateralTriangle extends Shape{
+//     public void area(int a){
+//         System.out.println((Math.sqrt(3)/4)*a*a);
+//     }
+// }
+public class TypeOfInheritance{
+    public static void main(String[] args){
+        bank.Accoubt acc = new bank.Account();
+        acc1.name = "customer2";
+        System.out.println(acc1.name);
+    }
+}
